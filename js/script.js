83ab2e6d74bec8feb2c99002474f7851fcb7357e
Samuel Anderson <samuela820@gmail.com>
2019-03-20 22:01:16 +0000

    Finish

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@
 /* global $ */
 
 $("#search-button").click(function(){
+    $.ajax({
+        url:https: "https:api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=cVRNyx7QJZX0F6nTWQ0XbssO1yb4KtSM"
+        method:"GET",
+        success:function(response){
+        
+        },
+    })
   
   
   
